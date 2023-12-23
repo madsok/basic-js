2aa745a0545da5e2976253ae8f78f6db954bd2dd
madsok <madsok25@gmail.com>
2023-12-23 17:46:22 +0300

    feat: done vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,106 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag = true) {
+    this.flag = flag;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const arr = alphabet.split('');
+
+    let str = string.split('');
+
+    let cleanStr = string.replaceAll(' ', '').toUpperCase().split('').filter(el => el.match(/^[a-zA-Z]/g));
+
+    let k = key;
+
+    if (k.length > cleanStr.length) {
+      k = k.slice(0, cleanStr.length).toUpperCase();
+    } 
+
+    if (k.length < cleanStr.length) {
+      for (let i = key.length; i < cleanStr.length; i += i) {
+        k = k + k;
+        if (k.length > cleanStr.length) {
+          k = k.slice(0, cleanStr.length).toUpperCase();
+          break;
+        }
+      }
+    }
+
+    k = k.toUpperCase();
+
+    let cyp =  cleanStr.map((el,i) => arr[(arr.indexOf(el) + arr.indexOf(k[i])) % 26]);
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i].match(/^[a-zA-Z]/g)) {
+      	str[i] = cyp[0];
+        cyp.splice(0,1);
+      }
+    }
+
+    if (this.flag) {
+      return str.join('').toUpperCase();
+    }
+
+    return str.reverse().join('').toUpperCase();
+  }
+  decrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const arr = alphabet.split('');
+
+    let str = string.split('');
+
+    let cleanStr = string.replaceAll(' ', '').toUpperCase().split('').filter(el => el.match(/^[a-zA-Z]/g));
+
+    let k = key;
+
+    if (k.length > cleanStr.length) {
+      k = k.slice(0, cleanStr.length).toUpperCase();
+    } 
+
+    if (k.length < cleanStr.length) {
+      for (let i = key.length; i < cleanStr.length; i += i) {
+        k = k + k;
+        if (k.length > cleanStr.length) {
+          k = k.slice(0, cleanStr.length).toUpperCase();
+          break;
+        }
+      }
+    }
+
+    k = k.toUpperCase();
+
+    let cyp =  cleanStr.map((el,i) => {
+      if (arr.indexOf(el) - arr.indexOf(k[i]) < 0) {
+          return arr[(arr.indexOf(el) - arr.indexOf(k[i]) + 26)];
+        } else {
+          return arr[(arr.indexOf(el) - arr.indexOf(k[i]) % 26)];
+        }
+    });
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str[i].match(/^[a-zA-Z]/g)) {
+      	str[i] = cyp[0];
+        cyp.splice(0,1);
+      }
+    }
+
+    if (this.flag) {
+      return str.join('').toUpperCase();
+    }
+
+    return str.reverse().join('').toUpperCase();
   }
 }
 
